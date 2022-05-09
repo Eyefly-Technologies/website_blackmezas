234fdb9bc26fe14fa98e40bdb1175057878c9e11
LucaBia <gian.luca.99@hotmail.com>
2022-05-09 00:21:07 -0600

    Home banner

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import styles from './styles.scss';
+import './styles.scss';
 import 'bulma/css/bulma.min.css';
+
+import logo from './logo.svg';
+import map from './map.png';
+import maskGroup from './MaskGroup.svg';
 
 
 const Home = () => (
@@ -9,11 +13,9 @@ const Home = () => (
         <section class="hero is-black is-fullheight">
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <p class="title">
-                        Fullheight hero
-                    </p>
-                    <p class="subtitle">
-                        Fullheight subtitle
+                    <img src={logo}/>
+                    <p class="slogan">
+                        Premium Coworking Environments
                     </p>
                 </div>
             </div>
@@ -43,10 +45,14 @@ const Home = () => (
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <div class="columns">
-                        <div class="column">1</div>
+                        <div class="column">
+                            {/* <img src={maskGroup} /> */}
+                            <p>
+                                Destructuración del trabajo al alza.
+                            </p>
+                        </div>
                         <div class="column">2</div>
                         <div class="column">3</div>
-                        <div class="column">4</div>
                     </div>
                 </div>
             </div>
@@ -54,49 +60,71 @@ const Home = () => (
 
         {/* La formula correcta */}
         <section class="hero is-black is-fullheight">
-            <div class="hero-head">
-                <p class="title">
-                    Una perspectiva continental
-                </p>
-            </div>
             <div class="hero-body">
+                <div class="container">
+                    <p class="title">
+                        La fórmula correcta
+                    </p>
+                </div>
                 <div class="container has-text-centered">
                     <div class="columns">
-                        <div class="column">1</div>
+                        <div class="column">
+                            {/* <img src={maskGroup} /> */}
+                            <p>
+                                Destructuración del trabajo al alza.
+                            </p>
+                        </div>
                         <div class="column">2</div>
-                        <div class="column">3</div>
-                        <div class="column">4</div>
+                    </div>
+                    <div class="columns">
+                        <div class="column">
+                            {/* <img src={maskGroup} /> */}
+                            <p>
+                                Destructuración del trabajo al alza.
+                            </p>
+                        </div>
+                        <div class="column">2</div>
                     </div>
                 </div>
             </div>
         </section>
 
         {/* Propuesta de valor */}
-        <section class="hero is-black is-fullheight">
+        <section class="hero is-white is-fullheight">
             <div class="hero-head">
                 <p class="title">
-                    Una perspectiva continental
+                    Propuesta de valor
                 </p>
             </div>
             <div class="hero-body">
                 <div class="container has-text-centered">
                     <div class="columns">
-                        <div class="column">1</div>
+                        <div class="column">
+                            {/* <img src={maskGroup} /> */}
+                            <p>
+                                Destructuración del trabajo al alza.
+                            </p>
+                        </div>
                         <div class="column">2</div>
-                        <div class="column">3</div>
-                        <div class="column">4</div>
+                    </div>
+                    <div class="columns">
+                        <div class="column">
+                            {/* <img src={maskGroup} /> */}
+                            <p>
+                                Destructuración del trabajo al alza.
+                            </p>
+                        </div>
+                        <div class="column">2</div>
                     </div>
                 </div>
             </div>
         </section>
 
         {/* Footer */}
-        <footer class="footer">
+        <footer class="footer test">
             <div class="content has-text-centered">
                 <p>
-                <strong>BlackMezas</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
-                <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
-                is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                    <strong>BlackMezas</strong> - © 2022 - All rights reserved
                 </p>
             </div>
         </footer>
