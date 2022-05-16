9e44125ce9fbe8c8954e2a8e17b01f529591b098
LucaBia <gian.luca.99@hotmail.com>
2022-05-16 13:32:51 -0600

    Avances Nosotos que aportamos

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,14 +3,16 @@ import './styles.css';
 // import Home from '../Home';
 // import Ubicaciones from '../Ubicaciones';
 // import Contactanos from '../Contactanos';
-import Asociarse from '../Asociarse';
+// import Asociarse from '../Asociarse';
+import Nosotros from '../Nosotros';
 
 function App() {
   return (
     // <Home />
     // <Contactanos/>
     // <Ubicaciones/>
-    <Asociarse />
+    // <Asociarse />
+    <Nosotros />
   );
 }
 
---
+++ src/components/Nosotros/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import './styles.scss';
+import 'bulma/css/bulma.min.css';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import Diploma from './Diploma.png';
+import Marker from './Marker.png';
+
+import Footer from '../Footer'
+
+
+const Nosotros = () => (
+    <>
+        {/* Acerca de nosotros */}
+        <section className="hero is-black is-fullheight">
+
+        </section>
+
+        {/* Por que BlackMezas */}
+        <section className="hero is-primary is-fullheight">
+            
+        </section>
+
+        {/* Nuestras instalaciones */}
+        <section className="hero is-white is-fullheight">
+            
+        </section>
+
+        {/* Que aportamos */}
+        <section className="hero is-black is-fullheight">
+            <div className="hero-body">
+                <div className='container has-text-centered'>
+                    <div className='columns qaportamos'>
+                        <div className='column qaportamos'>
+                            <div className='contQaportamos'>
+                                <img src={Diploma} alt='' />
+                                <p className='qaportamosTitle'>Experiencia</p>
+                                <p className='qaportamosDesc'>
+                                    Tenemos más de 60 años de experiencia combinada en desarrollos inmobiliarios 
+                                    residenciales y comerciales, así como productos y servicios relacionados.
+                                </p>
+                            </div>
+                        </div>
+                        <div className='column qaportamos'>
+                            <div className='contQaportamos'>
+                                <img src={Marker} alt='' />
+                                <p className='qaportamosTitle'>Referencia</p>
+                                <p className='qaportamosDesc'>
+                                    BlackMezas® posee experiencia en varios países de LATAM, 
+                                    de Europa y en Estados Unidos.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        {/* Galeria de fotos */}
+        <section className="hero is-white is-fullheight">
+            
+        </section>
+
+        {/* Contactanos */}
+        <section className="hero is-black is-fullheight">
+            {/* <div className='footerCon'>
+                <Footer/>
+            </div> */}
+        </section>
+
+    </>
+
+)
+
+export default(Nosotros);
