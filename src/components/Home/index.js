6779427653aa58ce35506592e5d7066ef5d7d95b
LucaBia <gian.luca.99@hotmail.com>
2022-05-19 01:08:34 -0600

    Modal for menu

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,8 +3,10 @@ import './styles.scss';
 import 'bulma/css/bulma.min.css';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
+import { Twirl as Hamburger } from 'hamburger-react';
+import Modal from 'react-modal';
 
-import Footer from '../Footer'
+import Footer from '../Footer';
 
 import logo from './logo.svg';
 import img1800_2018 from './1800-2018.png';
@@ -21,8 +23,56 @@ import Marker from './Marker.svg'
 
 
 
-const Home = () => (
+
+const modalStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      margin: 0,
+      padding: 0,
+      height: '101%',
+      width: '101%',
+      backgroundColor: 'black'
+    },
+};
+
+const Home = () => {
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+    function openModal() {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+    return (
     <div>
+        <div className='hamburger'>
+            <Hamburger
+                color="#FFFFFF" 
+                onToggle={ 
+                    toggled => {
+                        if (toggled) {
+                            openModal()
+                        } else {
+                            closeModal()
+                        }
+                    }
+                } style={{color: 'red'}} />
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={() => setIsOpen(false)}
+                contentLabel="Menu"
+                style={modalStyles}
+            >
+                <div>
+                    <p>HOLA</p>
+                </div>
+            </Modal>
+        </div>
+
         {/* Logo */}
         <section className="hero is-black is-fullheight">
             <div className="hero-body">
@@ -217,6 +267,7 @@ const Home = () => (
         <Footer />
     </div>
 )
+}
 
 
 export default(Home);
